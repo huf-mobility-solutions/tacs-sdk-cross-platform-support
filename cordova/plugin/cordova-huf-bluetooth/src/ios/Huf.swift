1f1ce5f066f90758f531d5618e221b4fea9fc454
Vivek Narasimha <vivek.narasimha@huf-group.com>
2020-08-19 15:20:43 +0200

    fix dispose bag uninitialized error

--- cordova/plugin/cordova-huf-bluetooth/src/ios/Huf.swift
+++ cordova/plugin/cordova-huf-bluetooth/src/ios/Huf.swift
@@ -2,166 +2,162 @@ import TACS
 import SecureAccessBLE
 
 @objc(Huf) class Huf : CDVPlugin {
-
-  private var tacsManager: TACSManager!
     
-  @objc(buildKeyring:) 
-  func buildKeyring(command: CDVInvokedUrlCommand) { 
-
-    print("Building Keyring")
-
-    let vehicleAccessGrantId: String = "MySampleAccessGrantId"
-    let queue = DispatchQueue(label: "com.hufsm.blehandling")
-    tacsManager = TACSManager(queue: queue)
-    let json = command.argument(at: 0) as! String
-    let data = json.data(using: .utf8)
-    let keyRing = try! JSONDecoder().decode(TACSKeyRing.self, from: data!)
+    private var tacsManager: TACSManager!
+    private var disposeBag: DisposeBag!
     
-    registerSubscriptions()
+    @objc(buildKeyring:)
+    func buildKeyring(command: CDVInvokedUrlCommand) {
         
-    // Prepare tacsmanager with vehicleAccessGrantId and appropriate keyring
-    let useAccessGrantResult = tacsManager.useAccessGrant(with: vehicleAccessGrantId, from: keyRing)
-    assert(useAccessGrantResult)
-
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-
-    // Set the plugin result to succeed.
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-     
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-  }
-
-  @objc(connectBle:) 
-  func connectBle(command: CDVInvokedUrlCommand) { 
-    print("TACS Connecting BLE")
-
-    // Connect
-    guard .poweredOn == tacsManager.bluetoothState.state else { return }
-    if case .connected = tacsManager.connectionChange.state { return }
-    // Start scanning for vehicles
-    tacsManager.startScanningWithTimeout()
-
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    
-    // Set the plugin result to succeed.
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-    
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-  }
-
-  @objc(disconnectBle:) 
-  func disconnectBle(command: CDVInvokedUrlCommand) { 
-    print("TACS Disconnecting BLE")
-
-    // Disconnect
-    guard .disconnected != tacsManager.connectionChange.state else { return }
-    tacsManager.disconnect()
-
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    
-    // Set the plugin result to succeed.
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-    
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-  }
-
-  @objc(executeLock:) 
-  func executeLock(command: CDVInvokedUrlCommand) { 
-    print("TACS Executing lock")
-
-    tacsManager.vehicleAccessManager.requestFeature(.lock)
-    tacsManager.vehicleAccessManager.requestFeature(.lockStatus)
-
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    
-    // Set the plugin result to succeed.
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-    
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-  }
-
-  @objc(executeUnlock:) 
-  func executeUnlock(command: CDVInvokedUrlCommand) { 
-    print("TACS Executing unlock")
-
-    tacsManager.vehicleAccessManager.requestFeature(.unlock)
-    tacsManager.vehicleAccessManager.requestFeature(.lockStatus)
-
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    
-    // Set the plugin result to succeed.
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-    
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
-  }
-    
-    
-  private func registerSubscriptions() {
-    let disposeBag = DisposeBag()
-
-    // Subscribe to bluetooth state signal
-    tacsManager.bluetoothState.subscribe { [weak self] bluetoothState in
-           self?.onBluetoothStateChange(bluetoothState)
-       }.disposed(by: disposeBag)
-       
-       // Subscribe to discovery change signal
-       tacsManager.discoveryChange.subscribe { [weak self] discoveryChange in
-           // handle discovery state changes
-           self?.onDiscoveryChange(discoveryChange)
-           }
-           .disposed(by: disposeBag) // add disposable to a disposeBag which will take care about removing subscriptions on deinit
-       
-       // Subscribe to connection change signal
-       tacsManager.connectionChange.subscribe { [weak self] connectionChange in
-           self?.onConnectionChange(connectionChange)
-           }
-           .disposed(by: disposeBag)
-       
-       // Subscribe to vehicle access change signal
-       tacsManager.vehicleAccessManager.vehicleAccessChange.subscribe { [weak self] vehicleAccessChange in
-           // Handle vehicle access changes
-           self?.onVehicleAccessFeatureChange(vehicleAccessChange)
-           }
-           .disposed(by: disposeBag)
-               
-       // Subscribe to telematics data change signal
-       tacsManager.telematicsManager.telematicsDataChange.subscribe { [weak self] telematicsDataChange in
-           self?.onTelematicsDataChange(telematicsDataChange)
-           }
-           .disposed(by: disposeBag)
-       
-       // Subscribe to location data change signal
-       tacsManager.telematicsManager.locationDataChange.subscribe { [weak self] locationDataChange in
-           self?.onLocationDataChange(locationDataChange)
-       }.disposed(by: disposeBag)
-       
-       //Subscribe to keyholder state change signal
-       tacsManager.keyholderManager.keyholderChange.subscribe { [weak self] change  in
-           self?.onKeyholderStatusChange(change)
-           }
-           .disposed(by: disposeBag)
+        print("Building Keyring")
+        
+        let vehicleAccessGrantId: String = "MySampleAccessGrantId"
+        let queue = DispatchQueue(label: "com.hufsm.blehandling")
+        tacsManager = TACSManager(queue: queue)
+        disposeBag = DisposeBag()
+        let json = command.argument(at: 0) as! String
+        let data = json.data(using: .utf8)
+        let keyRing = try! JSONDecoder().decode(TACSKeyRing.self, from: data!)
+        
+        registerSubscriptions()
+        
+        // Prepare tacsmanager with vehicleAccessGrantId and appropriate keyring
+        let useAccessGrantResult = tacsManager.useAccessGrant(with: vehicleAccessGrantId, from: keyRing)
+        assert(useAccessGrantResult)
+        
+        // Set the plugin result to fail.
+        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+        
+        // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        
+        // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
     
-  private func onBluetoothStateChange(_ bluetoothState: BluetoothState) {
-    // Reflect on ble device change by providing necessary feedback to the user.
-    // Running discoveries for vehicle or keyholder will automatically stop and notified via signals.
-    DispatchQueue.main.async { [weak self] in
-      print("Bluetooth state is")
-      print(bluetoothState)
+    @objc(connectBle:)
+    func connectBle(command: CDVInvokedUrlCommand) {
+        print("TACS Connecting BLE")
+        
+        // Connect
+        guard .poweredOn == tacsManager.bluetoothState.state else { return }
+        if case .connected = tacsManager.connectionChange.state { return }
+        // Start scanning for vehicles
+        tacsManager.startScanningWithTimeout()
+        
+        // Set the plugin result to fail.
+        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+        
+        // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        
+        // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
-  }
     
-  private func onDiscoveryChange(_ discoveryChange: TACS.DiscoveryChange) {
+    @objc(disconnectBle:)
+    func disconnectBle(command: CDVInvokedUrlCommand) {
+        print("TACS Disconnecting BLE")
+        
+        // Disconnect
+        guard .disconnected != tacsManager.connectionChange.state else { return }
+        tacsManager.disconnect()
+        
+        // Set the plugin result to fail.
+        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+        
+        // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        
+        // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+    
+    @objc(executeLock:)
+    func executeLock(command: CDVInvokedUrlCommand) {
+        print("TACS Executing lock")
+        
+        tacsManager.vehicleAccessManager.requestFeature(.lock)
+        tacsManager.vehicleAccessManager.requestFeature(.lockStatus)
+        
+        // Set the plugin result to fail.
+        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+        
+        // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        
+        // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+    
+    @objc(executeUnlock:)
+    func executeUnlock(command: CDVInvokedUrlCommand) {
+        print("TACS Executing unlock")
+        
+        tacsManager.vehicleAccessManager.requestFeature(.unlock)
+        tacsManager.vehicleAccessManager.requestFeature(.lockStatus)
+        
+        // Set the plugin result to fail.
+        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+        
+        // Set the plugin result to succeed.
+        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+        
+        // Send the function result back to Cordova.
+        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    }
+    
+    
+    private func registerSubscriptions() {
+        
+        // Subscribe to bluetooth state signal
+        tacsManager.bluetoothState.subscribe { [weak self] bluetoothState in
+            self?.onBluetoothStateChange(bluetoothState)
+        }.disposed(by: disposeBag)
+        
+        // Subscribe to discovery change signal
+        tacsManager.discoveryChange.subscribe { [weak self] discoveryChange in
+            // handle discovery state changes
+            self?.onDiscoveryChange(discoveryChange)
+        }.disposed(by: disposeBag) // add disposable to a disposeBag which will take care about removing subscriptions on deinit
+        
+        // Subscribe to connection change signal
+        tacsManager.connectionChange.subscribe { [weak self] connectionChange in
+            self?.onConnectionChange(connectionChange)
+        }.disposed(by: disposeBag)
+        
+        // Subscribe to vehicle access change signal
+        tacsManager.vehicleAccessManager.vehicleAccessChange.subscribe { [weak self] vehicleAccessChange in
+            // Handle vehicle access changes
+            self?.onVehicleAccessFeatureChange(vehicleAccessChange)
+        }.disposed(by: disposeBag)
+        
+        // Subscribe to telematics data change signal
+        tacsManager.telematicsManager.telematicsDataChange.subscribe { [weak self] telematicsDataChange in
+            self?.onTelematicsDataChange(telematicsDataChange)
+        }.disposed(by: disposeBag)
+        
+        // Subscribe to location data change signal
+        tacsManager.telematicsManager.locationDataChange.subscribe { [weak self] locationDataChange in
+            self?.onLocationDataChange(locationDataChange)
+        }.disposed(by: disposeBag)
+        
+        //Subscribe to keyholder state change signal
+        tacsManager.keyholderManager.keyholderChange.subscribe { [weak self] change  in
+            self?.onKeyholderStatusChange(change)
+        }.disposed(by: disposeBag)
+    }
+    
+    private func onBluetoothStateChange(_ bluetoothState: BluetoothState) {
+        // Reflect on ble device change by providing necessary feedback to the user.
+        // Running discoveries for vehicle or keyholder will automatically stop and notified via signals.
+        DispatchQueue.main.async { [weak self] in
+            print("Bluetooth state is")
+            print(bluetoothState)
+        }
+    }
+    
+    private func onDiscoveryChange(_ discoveryChange: TACS.DiscoveryChange) {
         switch discoveryChange.action {
         case .discoveryStarted(_):
             DispatchQueue.main.async { [weak self] in
@@ -172,7 +168,7 @@ import SecureAccessBLE
             tacsManager.connect()
         case .discoveryFailed:
             DispatchQueue.main.async { [weak self] in
-               print("Discovering failed...")
+                print("Discovering failed...")
             }
         default:
             break
@@ -189,7 +185,8 @@ import SecureAccessBLE
         }
         
         DispatchQueue.main.async {
-            
+            print("On connection change")
+            print(status)
         }
     }
     
@@ -237,8 +234,8 @@ import SecureAccessBLE
                 }
             }
         }
-
     }
+    
     private func onTelematicsDataChange(_ telematicsDataChange: TelematicsDataChange) {
         DispatchQueue.main.async {
             if case let .responseReceived(responses) = telematicsDataChange.action {
